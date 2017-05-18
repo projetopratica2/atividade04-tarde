88378d4c00ed504b49a654a6e0be719622109307
Gilmar Rodrigues dos Santos <gilmar@Gilmar.R>
2017-05-18 15:34:13 -0300

    ‘classe’

---
+++ src/Produto.java
@@ -0,0 +1,41 @@
+Produto.java
+
+public class Produto {
+	private long id;
+	private String titulo;
+	private String descricao;
+	private double preco;
+	private String nomeImagem;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getNomeImagem() {
+		return nomeImagem;
+	}
+	public void setNomeImagem(String nomeImagem) {
+		this.nomeImagem = nomeImagem;
+	}
+	
+
+}
